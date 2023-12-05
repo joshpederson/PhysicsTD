4e042bfefb47df267d41d8ce00841128f7149315
joshpederson <pedersonj8674@my.uwstout.edu>
2023-12-05 13:19:29 -0600

    Eh

--- enemy.py
+++ enemy.py
@@ -1,2 +1,7 @@
 import pygame
 
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, pos, image):
+        self.image = image
+        self.rec = self.image.getRect()
+        self.rec.center = pos
