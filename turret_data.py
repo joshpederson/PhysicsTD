d81194dee48ca3e11798945f34fee2ebd7e85062
joshpederson <pedersonj8674@my.uwstout.edu>
2023-12-12 08:17:00 -0600

    updated cooldowns

--- turret_data.py
+++ turret_data.py
@@ -2,21 +2,21 @@ TURRET_DATA = [
   {
     #1
     "range": 90,
-    "cooldown": 15,
+    "cooldown": 150,
   },
   {
     #2
     "range": 110,
-    "cooldown": 1200,
+    "cooldown": 120,
   },
   {
     #3
     "range": 125,
-    "cooldown": 1000,
+    "cooldown": 100,
   },
   {
     #4
     "range": 150,
-    "cooldown": 900,
+    "cooldown": 90,
   }
 ]
